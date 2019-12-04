c3179d52ab5aa70a81aff010f678506c5de995dc
N4THB3N <36906451+N4THB3N@users.noreply.github.com>
2019-12-04 17:35:29 -0600

    conexion con heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = require('./app');
 var port = process.env.PORT || 4022;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Zoo', {useNewUrlParser: true})
+mongoose.connect('mongodb+srv://nathan:<Natan-11-Dios>@cluster0-1pm9t.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true})
 .then((err,res)=>{
     console.log('Swited to Zoo DB');
     app.listen(port, () =>{
